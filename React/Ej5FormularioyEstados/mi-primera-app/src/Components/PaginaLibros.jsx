0bc506a9d2faf36cb85f489878edef5ece9cc6f6
MatiBalda20 <matiasbalda04@gmail.com>
2025-09-08 20:07:45 -0300

    Adapto libros y pagina libro para usar API

--- React/Ej5FormularioyEstados/mi-primera-app/src/Components/PaginaLibros.jsx
+++ React/Ej5FormularioyEstados/mi-primera-app/src/Components/PaginaLibros.jsx
@@ -1,6 +1,9 @@
 import Libro from "./Libro";
+import useLibrosPorGenero from "../hooks/useLibrosPorGenero"; 
 
-export default function PaginaLibros({ tituloPagina, libros }) {
+export default function PaginaLibros({ tituloPagina, genero }) {
+    const { libros, loading } = useLibrosPorGenero(genero, 12);
+
     // Divide libros en grupos de 3
     const grupos = [];
     for (let i = 0; i < libros.length; i += 3) {
@@ -9,18 +12,24 @@ export default function PaginaLibros({ tituloPagina, libros }) {
 
     return (
         <main className="p-6">
-            <h2 className="text-3xl font-bold text-black mb-6">{tituloPagina}:</h2>
+        <h2 className="text-3xl font-bold text-black mb-6">
+            {tituloPagina}:
+        </h2>
 
-            {grupos.map((grupo, index) => (
-                <section
-                    key={index}
-                    className="flex gap-[10vh] p-5 h-[40vh] bg-[#fcfcfc] flex-wrap justify-center mt-4"
-                >
-                    {grupo.map((libro, i) => (
-                        <Libro key={i} {...libro} />
-                    ))}
-                </section>
-            ))}
+        {loading ? (
+            <p className="text-gray-600">Cargando libros...</p>
+        ) : (
+            grupos.map((grupo, index) => (
+            <section
+                key={index}
+                className="flex gap-[10vh] p-5 h-[40vh] bg-[#fcfcfc] flex-wrap justify-center mt-4"
+            >
+                {grupo.map((libro, i) => (
+                <Libro key={i} {...libro} />
+                ))}
+            </section>
+            ))
+        )}
         </main>
     );
 }
